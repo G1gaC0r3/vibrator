c0af5f9f012f4a313abc5979145ad6682780d0db
John Doe <johndoe@example.com>
2024-05-24 18:54:49 +0700

    update by viktor

--- inventaris_shop/resources/views/dashboard.blade.php
+++ inventaris_shop/resources/views/dashboard.blade.php
@@ -1,17 +1,122 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <title>Website Inventaris</title>
+</head>
+
+<body>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+            <div class="logo"><span>Inventaris</span></div>
+        <ul class="side-menu">
+            <li><a href="stok.blade.php"><i class='bx bxs-dashboard'></i>Stok Barang</a></li>
+            <li><a href="masuk.blade.php"><i class='bx bx-arrow-to-left'></i>Barang Masuk</a></li>
+            <li ><a href="#"><i class='bx bx-arrow-to-right'></i>Barang Keluar</a></li>
+            <li><a href="#"><i class='bx bx-group'></i>Users</a></li>
+        </ul>
+        <ul class="side-menu">
+            <li>
+                <a href="#" class="logout">
+                    <i class='bx bx-log-out-circle'></i>
+                    Logout
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- End of Sidebar -->
+
+    <!-- Main Content -->
+    <div class="content">
+        <!-- Navbar -->
+        <nav>
+            <i class='bx bx-menu'></i>
+            <form action="#">
+                <div class="form-input">
+                    <input type="search" placeholder="Search...">
+                    <button class="search-btn" type="submit"><i class='bx bx-search'></i></button>
+                </div>
+            </form>
+            <input type="checkbox" id="theme-toggle" hidden>
+            <label for="theme-toggle" class="theme-toggle"></label>
+            <a href="#" class="profile">
+                <img src="profiel.png" alt="">
+            </a>
+        </nav>
+
+        <!-- End of Navbar -->
+
+        <main>
+            <div class="header">
+                <div class="left">
+                    <h1>Stok Barang</h1>
+                    
                 </div>
             </div>
-        </div>
-    </div>
-</x-app-layout>
+
+            <!-- Insights -->
+            <ul class="insights">
+                <li>
+                    <i class='bx bx-arrow-to-right' style="color: rgb(24, 141, 180);"></i>
+                    <span class="info">
+                        <h3>
+                            41
+                        </h3>
+                        <p>Total Barang Masuk</p>
+                    </span>
+                </li>
+                <li><i class='bx bx-arrow-to-left' style="color: rgb(207, 164, 10);"></i>
+                    <span class="info">
+                        <h3>
+                            20
+                        </h3>
+                        <p>Total Barang Keluar</p>
+                    </span>
+                </li>
+            </ul>
+            <!-- End of Insights -->
+
+            <div class="bottom-data">
+                <div class="orders">
+                    <div class="header">
+                        <i class='bx bx-receipt'></i>
+                        <h3>Daftar Barang</h3>
+                        <i class='bx bx-filter'></i>
+                        <i class='bx bx-search'></i>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Kode Barang</th>
+                                <th>Nama Barang</th>
+                                <th>Gambar</th>
+                                <th>Deskripsi</th>
+                                <th>Jumlah</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>231</td>
+                                <td>Iphone</td>
+                                <td></td>
+                                <td>Kurang Bagus</td>
+                                <td>53</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
+    </div>  
+            
+    
+    <script src="index.js"></script>
+</body>
+
+</html>
