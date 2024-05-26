bae9ebe10f3eddcf4d85ef6168dc8d24b78dfd6a
Kajung <Kajung@example.com>
2024-05-26 15:50:13 +0800

    pliss

--- inventaris_shop/resources/views/welcome.blade.php
+++ inventaris_shop/resources/views/welcome.blade.php
@@ -11,15 +11,18 @@
             <h1>Welcome to Inventory</h1>
             <p>Inventory Management System</p>
             @if (Route::has('login'))
-                <div>
-                    @auth
-                        <a href="{{ route('login') }}" class="btn"><span class="span"></span>Log in</a>
-                                               @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="btn"><span class="span"></span>Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div>
+                @auth
+                    <a href="{{ url('dashboard') }}" class="btn"><span class="span"></span>Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="btn"><span class="span"></span>Log in</a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="btn"><span class="span"></span>Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+        
         </div>
     </div>
     </body>
