325cff3eeab9aa644cee1bf2f494317fe1386bcf
Kajung <Kajung@example.com>
2024-05-28 13:56:30 +0800

    bukan aku sumpah

--- inventaris_shop/app/Http/Controllers/UserController.php
+++ inventaris_shop/app/Http/Controllers/UserController.php
@@ -3,41 +3,82 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function showUsers()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        $userP = Auth::user();
-        return view('users', ['userP' => $userP]);
+        //
     }
 
-    public function updateProfile(Request $request)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email',
-            'phone' => 'required',
-            'birthdate' => 'required|date',
-            'profile_picture' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
+        //
+    }
 
-        $userP = Auth::user(); // mengganti $user menjadi $userP
-        $userP->name = $request->name;
-        $userP->email = $request->email;
-        $userP->phone = $request->phone;
-        $userP->birthdate = $request->birthdate;
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-        if ($request->hasFile('profile_picture')) {
-            $imageName = time().'.'.$request->profile_picture->extension();  
-            $request->profile_picture->move(public_path('images'), $imageName);
-            $userP->profile_picture = $imageName;
-        }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-        $userP->save();
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-        return redirect()->back()->with('status', 'profile-updated');
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
-
