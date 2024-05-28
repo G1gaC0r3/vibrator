afd5d3afe419948ec0b9fe4e7007f42a5fc80d0c
Your Name <you@example.com>
2024-05-28 19:25:26 +0800

    pp

--- inventaris_shop/resources/views/users.blade.php
+++ inventaris_shop/resources/views/users.blade.php
@@ -67,11 +67,11 @@
                 <table class="table">
                     <thead>
                         <tr>
-                            <th>Nama</th>
-                            <th>Email</th>
-                            <th>Nomor HP</th>
-                            <th>Tanggal Lahir</th>
-                            <th>Foto Profil</th>
+                            <th style="color: #696969;"><strong>Nama</strong></th>
+                            <th style="color: #696969;"><strong>Email</strong></th>
+                            <th style="color: #696969;"><strong>Nomor HP</strong></th>
+                            <th style="color: #696969;"><strong>Tanggal Lahir</strong></th>
+                            <td style="color: #696969;"><strong>Foto Profile</strong></td>
                         </tr>
                     </thead>
                     <tbody>
@@ -89,7 +89,7 @@
             @endif
         </div>
 
-        <div class="update-profile">
+        <div class="saved-profile">
             <div class="form-and-preview" style="display: flex; justify-content: space-between; flex-direction:column">
                 <div style="display: flex; justify-content:center">
                     <h3>Data User</h3>
@@ -97,11 +97,11 @@
                 <table class="table-saved">
                     <thead>
                         <tr>
-                            <th>Nama</th>
-                            <th>Email</th>
-                            <th>Nomor HP</th>
-                            <th>Tanggal Lahir</th>
-                            <td>Foto Profile</th>
+                            <th style="color: #696969;"><strong>Nama</strong></th>
+                            <th style="color: #696969;"><strong>Email</strong></th>
+                            <th style="color: #696969;"><strong>Nomor HP</strong></th>
+                            <th style="color: #696969;"><strong>Tanggal Lahir</strong></th>
+                            <td style="color: #696969;"><strong>Foto Profile</strong></td>
                         </tr>
                     </thead>
                     <tbody>
